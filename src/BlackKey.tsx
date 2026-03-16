0f7aab4432fb1c42cd36d82ce7c9f5dd93ab0e60
Andrew Sexton <drew.bks@gmail.com>
2026-03-15 22:49:58 -0400

    Added proper mouse down and up functionality to keys

--- src/BlackKey.tsx
+++ src/BlackKey.tsx
@@ -9,17 +9,16 @@ function BlackKey(props: BlackKeyProps) {
   const synth = new Tone.Synth().toDestination(); 
 
   const OnKeyPressed = () => {
-    console.log(props.note + " pressed!")
+    synth.triggerAttack(props.note);
+  }
 
-    const now = Tone.now();
-    
-    synth.triggerAttack(props.note, now);
-    synth.triggerRelease(now+0.2);
+  const OnKeyReleased = () => {
+    synth.triggerRelease();
   }
 
   return (
     <div className="w-0 z-10 flex justify-center overflow-visible">
-      <div onClick={OnKeyPressed} className="shrink-0 w-10 h-42 outline-4 outline-black flex justify-center text-lg bg-black rounded-lg mx-0.5 cursor-pointer hover:bg-neutral-800 transition-colors">
+      <div onMouseDown={OnKeyPressed} onMouseUp={OnKeyReleased} onMouseLeave={OnKeyReleased} className="shrink-0 w-10 h-42 outline-4 outline-black flex justify-center text-lg bg-black rounded-lg mx-0.5 cursor-pointer hover:bg-neutral-800 transition-colors">
         <div className="self-end text-white text-center select-none">
           {props.note}
         </div>
--- src/WhiteKey.tsx
+++ src/WhiteKey.tsx
@@ -9,17 +9,16 @@ function WhiteKey(props: WhiteKeyProps) {
   const synth = new Tone.Synth().toDestination(); 
 
   const OnKeyPressed = () => {
-    console.log(props.note + " pressed!")
+    synth.triggerAttack(props.note);
+  }
 
-    const now = Tone.now();
-
-    synth.triggerAttack(props.note, now);
-    synth.triggerRelease(now+0.2);
+  const OnKeyReleased = () => {
+    synth.triggerRelease();
   }
 
   return (
     <>
-      <div onClick={OnKeyPressed} className="w-14 h-70 outline-4 outline-black flex justify-center text-lg bg-white rounded-lg mx-0.5 z-0 cursor-pointer hover:bg-gray-200 transition-colors">
+      <div onMouseDown={OnKeyPressed} onMouseUp={OnKeyReleased} onMouseLeave={OnKeyReleased} className="w-14 h-70 outline-4 outline-black flex justify-center text-lg bg-white rounded-lg mx-0.5 z-0 cursor-pointer hover:bg-gray-200 transition-colors">
         <div className="self-end select-none">
           {props.note}
         </div>
