ede78802ba1ea8015e71062733e5703c4d3023f8
Andrew Sexton <drew.bks@gmail.com>
2026-03-22 20:18:58 -0400

    Made button go red when it changes too

--- src/PlayButton.tsx
+++ src/PlayButton.tsx
@@ -6,7 +6,7 @@ function PlayButton() {
 
   return (
     <div className="w-full flex justify-center items-center text-3xl h-14">
-      <button onClick={() => setPlayChord(!playChord)} className="w-60 h-full bg-linear-to-br from-green-400 to-green-800 rounded-xl shadow-green-950 shadow-2xl cursor-pointer transition-all brightness-100 hover:brightness-90 active:brightness-75">
+      <button onClick={() => setPlayChord(!playChord)} className={`w-60 h-full ${playChord ? "bg-linear-to-br from-red-400 to-red-800" : "bg-linear-to-br from-green-400 to-green-800"} rounded-xl shadow-green-950 shadow-2xl cursor-pointer transition-all brightness-100 hover:brightness-90 active:brightness-75`}>
         {playChord ? "Stop chord" : "Play chord"}
       </button>
     </div>
